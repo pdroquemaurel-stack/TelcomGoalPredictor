fc444b0ded360411801fa84fe08db9fd003fb41c
pdroquemaurel-stack <p.d.roquemaurel@gmail.com>
2026-03-17 15:22:10 +0000

    Change profile.id to profile.userId in updateMany

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -36,7 +36,7 @@ export default async function HomePage() {
 
   const recapUpdate = await prisma.profile.updateMany({
     where: {
-      id: profile.id,
+      userId: profile.userId,
       OR: [
         { lastDailyRecapSeenAt: null },
         { lastDailyRecapSeenAt: { lt: todayRange.start } },
